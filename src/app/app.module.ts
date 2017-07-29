f51b7106b31bc9667730fdb70aaaf40814cd5d97
Regan Lawton <reganlawton92@gmail.com>
2017-07-29 12:33:26 +0930

    New pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {MapPage} from "../pages/map/map";
+import {ProfilePage} from "../pages/profile/profile";
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    MapPage,
+    ProfilePage,
     HomePage,
     TabsPage
   ],
@@ -26,8 +26,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    MapPage,
+    ProfilePage,
     HomePage,
     TabsPage
   ],
